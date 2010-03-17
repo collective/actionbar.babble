b2fb7298c46666a81555f67316899bd24a0239b5
jcbrand <jcbrand@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2010-03-17 13:58:38 +0000

    removed superfluous javascript

--- babble/feeder/browser/javascript/babblefeeder.js
+++ babble/feeder/browser/javascript/babblefeeder.js
@@ -11,23 +11,6 @@ function updateOpenChatsCookie(username, title) {
 
 jQuery(document).ready(function() {
 
-    jQuery("#chatpanel").adjustPanel(); 
-
-    //Each time the viewport is adjusted/resized, execute the function
-    jQuery(window).resize(function () {
-        jQuery("#chatpanel").adjustPanel();
-    });
-
-    //Click event outside of subpanel
-    jQuery(document).click(function() { 
-        jQuery(".subpanel").hide(); 
-        jQuery("#footpanel li a").removeClass('active'); //remove active class on subpanel trigger
-    });
-    jQuery('.subpanel ul').click(function(e) {
-        e.stopPropagation(); //Prevents the subpanel ul from closing on click
-    });
-
-    //Click event on Chat Panel
     jQuery("#chatpanel a:first").click(function() { 
         title = 'bottomfeeder_online_contacts';
         var chatbox = jQuery('#chatbox_'+title);
@@ -66,13 +49,6 @@ jQuery(document).ready(function() {
             chatbox.show();
         }
         return false; //Prevent browser jump to link anchor
-    });
-
-    //Show/Hide delete icons on Alert Panel
-    jQuery("#alertpanel li").hover(function() {
-        jQuery(this).find("a.delete").css({'visibility': 'visible'}); //Show delete icon on hover
-    },function() {
-        jQuery(this).find("a.delete").css({'visibility': 'hidden'}); //Hide delete icon on hover out
     });
 });
 
