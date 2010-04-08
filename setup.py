bf6dafceb83e3de355dd56f8d9523db9c0d695ca
jcbrand <jcbrand@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2010-04-08 10:39:14 +0000

    Updated setup.py to handle the new 'src' dir and rename

--- setup.py
+++ setup.py
@@ -3,35 +3,37 @@ import os
 
 version = '1.0'
 
-setup(name='babble.feeder',
-      version=version,
-      description="A actionbar.panel integration package for the Babble instant messaging system.",
-      long_description=open("README.txt").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
-      # Get more strings from
-      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      classifiers=[
-        "Framework :: Plone",
-        "Programming Language :: Python",
-        ],
-      keywords='',
-      author='',
-      author_email='',
-      url='http://svn.plone.org/svn/collective/',
-      license='GPL',
-      packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['babble'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'setuptools',
-          'babble.client',
-          'actionbar.panel',
-      ],
-      entry_points="""
-      [z3c.autoinclude.plugin]
-      target = plone
-      """,
-      setup_requires=["PasteScript"],
-      paster_plugins=["ZopeSkel"],
-      )
+setup(
+    name='actionbar.babble',
+    version=version,
+    description="A actionbar.panel integration package for the Babble instant messaging system.",
+    long_description=open("README.txt").read() + "\n" +
+                    open(os.path.join("docs", "HISTORY.txt")).read(),
+    # Get more strings from
+    # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+    "Framework :: Plone",
+    "Programming Language :: Python",
+    ],
+    keywords='',
+    author='',
+    author_email='',
+    url='http://svn.plone.org/svn/collective/',
+    license='GPL',
+    packages=find_packages('src', exclude=['ez_setup']),
+    package_dir = {'' : 'src'},
+    namespace_packages=['actionbar'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'actionbar.client',
+        'actionbar.panel',
+    ],
+    entry_points="""
+    [z3c.autoinclude.plugin]
+    target = plone
+    """,
+    setup_requires=["PasteScript"],
+    paster_plugins=["ZopeSkel"],
+    )
